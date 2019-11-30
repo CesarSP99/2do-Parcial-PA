f203fcd3e33c090df97a44ffd06c05ce4a450d65
CesarSP <cesarandres00@gmail.com>
2019-11-30 09:58:00 -0400

    Prueba para el método Spend añadido

--- Salazar/PruebasUnitarias/UnitTest1.cs
+++ Salazar/PruebasUnitarias/UnitTest1.cs
@@ -27,5 +27,25 @@ namespace PruebasUnitarias
             Assert.AreEqual(valoresperado, valorobtenido);
 
         }
+        [TestMethod]
+        public void PruebaMetodoSpend()
+        {
+            ///Organizar
+
+            Person person = new Person("Cesar", 100);
+            //Voy a gastar 50 bs, el valor esperado seria 50
+            double valoresperado = 50;
+
+            ///Actuar
+
+            person.Spend(50);
+            double valorobtenido = person.SaveMoney;
+
+            ///Afirmar
+
+            //Comprobando los valores
+            Assert.AreEqual(valoresperado, valorobtenido);
+
+        }
     }
 }
