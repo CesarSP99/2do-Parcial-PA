fb5ee4479808fd71e13ba962c780e76c315a71a9
CesarSP <cesarandres00@gmail.com>
2019-11-30 09:55:09 -0400

    Prueba para Metodo Save añadida

---
+++ Salazar/PruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Cesar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PruebaMetodoSave()
+        {
+            ///Organizar
+            
+            Person person = new Person("Cesar", 100);
+            //Voy a guardar 50 bs, el valor esperado seria 150
+            double valoresperado = 150;
+
+            ///Actuar
+
+            person.Save(50);
+            double valorobtenido = person.SaveMoney;
+
+            ///Afirmar
+
+            //Comprobando los valores
+            Assert.AreEqual(valoresperado, valorobtenido);
+
+        }
+    }
+}
